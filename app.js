4df91181e150f905aeb9a03d7cc56c8f218ff167
Tushar Gupta <tushar_19@hotmail.com>
2019-03-05 20:54:54 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var faker = require('faker');
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'tushar1907',  //your username
+  database : 'join_us'         //the name of your db
+});
+
+
+// Selecting data
+// var q = 'select * from users';
+// connection.query(q, function (error, results, fields) {
+//   if (error) throw error;
+//   console.log(results);
+// });
+
+// Inserting data
+// var q = 'insert into users (email) values ("tushar_19@hotmail.com")';
+// connection.query(q, function (error, results, fields) {
+//   if (error) throw error;
+//   console.log(results);
+// });
+
+var person = {email: faker.internet.email()};
+
+//var q = 'insert into users (email) values ("tushar_19@hotmail.com")';
+connection.query('insert into users set ?',person, function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
+connection.end();
---
+++ schema.sql
@@ -0,0 +1,4 @@
+create table users (
+email varchar(255) primary key,
+created_at timestamp default now());
+
