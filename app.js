3710c9848d60529ae6b6d66a97b69df317e75614
tushar1907 <gupta.tus@husky.neu.edu>
2019-03-05 21:16:46 +0000

    500Users added

--- app.js
+++ app.js
@@ -23,12 +23,33 @@ var connection = mysql.createConnection({
 //   console.log(results);
 // });
 
-var person = {email: faker.internet.email()};
+// var person = {
+//   email: faker.internet.email(),
+//   created_at: faker.date.past()
+// };
 
-//var q = 'insert into users (email) values ("tushar_19@hotmail.com")';
-connection.query('insert into users set ?',person, function (error, results, fields) {
-  if (error) throw error;
-  console.log(results);
+// //var q = 'insert into users (email) values ("tushar_19@hotmail.com")';
+// connection.query('insert into users set ?',person, function (error, results, fields) {
+//   if (error) throw error;
+//   console.log(results);
+// });
+
+
+var data = [];
+for(var i = 0; i < 500; i++){
+    data.push([
+        faker.internet.email(),
+        faker.date.past()
+    ]);
+}
+ 
+ 
+var q = 'INSERT INTO users (email, created_at) VALUES ?';
+ 
+connection.query(q, [data], function(err, result) {
+  console.log(err);
+  console.log(result);
 });
+ 
 
-connection.end();
+
