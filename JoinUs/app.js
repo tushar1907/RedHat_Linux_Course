62858058e9160b10e494184d6e014ba1d3fb91af
tushar1907 <gupta.tus@husky.neu.edu>
2019-03-06 23:02:57 +0000

    Application done

--- JoinUs/app.js
+++ JoinUs/app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 var mysql = require('mysql');
+
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -9,13 +10,28 @@ var connection = mysql.createConnection({
   database : 'join_us'         //the name of your db
 });
 
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({exended: true}));
+app.use(express.static(__dirname + "/public"));
+
 
 app.get("/", function(req, res){
      var q = 'SELECT COUNT(*) as count FROM users';
      connection.query(q, function (error, results) {
      if (error) throw error;
-     var msg = "We have " + results[0].count + " users";
-     res.send(msg);
+     var count = results[0].count;
+     //var msg = "We have " + results[0].count + " users";
+     //res.send(msg);
+     res.render('home',{data: count});
+     });
+});
+
+app.post('/register', function(req,res){
+     var person = {email: req.body.email};
+     connection.query('INSERT INTO users SET ?', person, function(err, result) {
+     console.log(err);
+     console.log(result);
+     res.redirect("/");
      });
 });
 
